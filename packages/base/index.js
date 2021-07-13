7cc1b9742e87c909f8b4303985d56300a5ce9e5e
I ekwaL V <ekwal@protonmail.com>
2021-07-13 19:58:03 +0300

    fix: order `publishConfig` in `package.json` files

--- packages/base/index.js
+++ packages/base/index.js
@@ -50,6 +50,7 @@ module.exports = {
               'funding',
               'author',
               'type',
+              'publishConfig',
               'files',
               'exports',
               'main',
